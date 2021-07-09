f1b055cce4c53054a65a2b7284e3ba1aa83da143
StanislavSmolyanko <st.smolyanko@gmail.com>
2021-07-09 19:54:15 +0300

    added modal animation

--- js/popup-feedback.js
+++ js/popup-feedback.js
@@ -1,7 +1,10 @@
 let feedbackButton = document.querySelector('.contacts__link-help');
 let feedbackModal = document.querySelector('.modal-feedback');
 let feedbackCloseButton = feedbackModal.querySelector('.modal__close-btn');
+let feedbackForm = document.querySelector('.modal__form');
 let feedbackName = feedbackModal.querySelector('.name-input');
+let feedbackEmail = feedbackModal.querySelector('.email-input');
+let feedbackText = feedbackModal.querySelector('.textarea-input');
 
 feedbackButton.addEventListener('click', function(evt){
   evt.preventDefault();
@@ -12,11 +15,22 @@ feedbackButton.addEventListener('click', function(evt){
 feedbackCloseButton.addEventListener('click', function(evt){
   evt.preventDefault();
   feedbackModal.classList.add('off');
+  feedbackModal.classList.remove('modal-error');
 });
 
 window.addEventListener('keydown', function(evt){
   if (evt.key === 'Esc' || evt.key === 'Escape') {
     evt.preventDefault();
     feedbackModal.classList.add('off');
+    feedbackModal.classList.remove('modal-error');
     };
+});
+
+feedbackForm.addEventListener('submit', function (evt) {
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackText.value) {
+    evt.preventDefault();
+    feedbackModal.classList.remove('modal-error');
+    feedbackModal.offsetWidth = feedbackModal.offsetWidth;
+    feedbackModal.classList.add('modal-error');
+  }
 });
